0343e77963dadc16e9226812111f595f3db39cef
Maham Shahid <mahum.shahid934@gmail.com>
2018-08-10 21:17:25 +0500

    Cleaned up the app-log component

--- src/app/components/app-log/app-log.component.ts
+++ src/app/components/app-log/app-log.component.ts
@@ -1,29 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { AppMessage } from '../../appMessages.interface';
 import { MessageLogService } from '../../services/messageLog/messageLog.service';
 
 @Component({
-  selector: 'app-app-log',
+  selector: 'app-log',
   templateUrl: './app-log.component.html',
   styleUrls: ['./app-log.component.css']
 })
 export class AppLogComponent implements OnInit {
 
-  public message$: Observable<AppMessage[]>;
-
   constructor(public log: MessageLogService) {
-    this.message$ = new Observable<AppMessage[]>();
   }
 
-  ngOnInit() {
-    // console.log(this.log.getMessages());
-    this.message$ = of(this.log.getMessages());
-    // console.log(this.message$);
-  }
-
-  clearMessages() {
-    this.log.clearMessageLog();
-  }
+  ngOnInit() {  }
 
 }
