c467cfd44ee12ef1fe9c93342b29c43def183015
Maham Shahid <mahum.shahid934@gmail.com>
2018-08-11 21:01:34 +0500

    Files cleanup

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero.interface';
+import { HeroService } from '../../services/heroes/hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  topHeroes: Hero[];
+  limitTopHeroes: number;
+
+  constructor(private service: HeroService) {
+    this.topHeroes = [];
+    this.limitTopHeroes = 4;
+  }
+
+  ngOnInit() {
+    this.service.getData(this.limitTopHeroes)
+      .subscribe((response) => {
+        this.topHeroes = response;
+      });
+  }
+
+}
